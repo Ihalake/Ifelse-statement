e3512e1966eac877a802ae2daa14f3d5203b3ebb
Issene Halake <Issene Halake@IHALAKE>
2023-03-30 18:46:25 +0300

    Add project files.

---
+++ Ifelse statement/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Enter your marks");
+int maths = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter your marks");
+int biology = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter your marks");
+int physics = Convert.ToInt32(Console.ReadLine());
+
+int result = (maths + biology + physics) / 3;
+
+if (result < 35)
+{
+    Console.Write("Fail");
+}
+else if (result >= 35 && result <= 50)
+{
+    Console.Write("Pass class");
+}
+else if (result > 50 && result <= 60)
+{
+    Console.Write("Second class");
+}
+else if (result > 60 && result <= 70)
+{
+    Console.Write("First class");
+}
+else     
+{
+    Console.Write("Distinction class");
+}
